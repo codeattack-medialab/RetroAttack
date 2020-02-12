a5bbb7bfdf1480a182e6b8f79fe52f2d2183bf4a
Roberto <roberto@zenvoid.org>
2020-02-12 17:57:32 +0100

    Game loader skips missing game numbers

--- game/global.gd
+++ game/global.gd
@@ -1,11 +1,12 @@
 extends Node
 
 var current_scene
-var current_game := 1
+var current_game_idx := 0
 var mlp_facade := false
 var mlp_facade_size := Vector2(192, 157)
 var saved_window_size: Vector2
 var saved_window_position: Vector2
+var game_list := []
 
 
 func _ready():
@@ -13,6 +14,16 @@ func _ready():
 
 	var root = get_tree().get_root()
 	current_scene = root.get_child(root.get_child_count() - 1)
+
+	var not_found := 0
+	var game_id := 1
+	while not_found < 20:
+		if ResourceLoader.exists("res://scenes/game%d.tscn" % game_id):
+			game_list.append(game_id)
+			not_found = 0
+		else:
+			not_found += 1
+		game_id += 1
 
 
 func go_to_scene(new_scene):
@@ -27,10 +38,10 @@ func _deferred_go_to_scene(new_scene):
 
 
 func next_game():
-	current_game += 1
-	if not ResourceLoader.exists("res://scenes/game%d.tscn" % current_game):
-		current_game = 1
-	go_to_scene("res://scenes/game%d.tscn" % current_game)
+	current_game_idx += 1
+	if current_game_idx >= game_list.size():
+		current_game_idx = 0
+	go_to_scene("res://scenes/game%d.tscn" % game_list[current_game_idx])
 
 
 func _unhandled_input(event):
